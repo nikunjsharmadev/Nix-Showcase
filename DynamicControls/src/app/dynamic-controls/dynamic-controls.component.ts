c7000f114e25009ba2ab72c2d3847394b06a9ee6
Nix <nikunjsharmadev@gmail.com>
2026-05-06 03:46:42 -0300

    dynamic-controls: added initial files

---
+++ DynamicControls/src/app/dynamic-controls/dynamic-controls.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { DynamicControlService } from '../core/services';
+import { Observable, of } from 'rxjs';
+import { DynamicControl } from '../core/models';
+
+@Component({
+  standalone: false,
+  selector: 'app-dynamic-controls',
+  templateUrl: './dynamic-controls.component.html',
+  styleUrl: './dynamic-controls.component.css'
+})
+export class DynamicControlsComponent implements OnInit {
+  public dynamicControls$: Observable<DynamicControl[]> = of([]);
+  constructor(private dynamicControlService: DynamicControlService) {}
+  ngOnInit(): void {
+    this.dynamicControls$ = this.dynamicControlService.getDynamicControls();
+  }
+}
