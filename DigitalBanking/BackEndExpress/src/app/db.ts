bc8fddd9efbd27db31a4ac6d2953d56022f9293c
Nix <nikunjsharmadev@gmail.com>
2026-07-28 15:20:44 -0300

    chnges for type mismatch dev

--- DigitalBanking/BackEndExpress/src/app/db.ts
+++ DigitalBanking/BackEndExpress/src/app/db.ts
@@ -2,14 +2,14 @@ import mongoose from 'mongoose';
 import { ENV, MONGOOSE_OPTIONS, PROCESS } from './constants/const.js';
 // DATABASE
 export async function Database() {
-  const dbUrl = ENV.ENV === 'dev' ? ENV.DB_DEV! : ENV.DB_PROD!;
+  const dbUrl = ENV.TYPE === 'dev' ? ENV.DB_DEV! : ENV.DB_PROD!;
   if (!dbUrl) throw new Error('database url missing');
   try {
     await mongoose.connect(dbUrl, MONGOOSE_OPTIONS);
     console.info('✅ Database connection Success');
     if (mongoose.connection.db) {
       mongoose.connection.db.admin().command({ ping: 1 });
-      console.info('Pinged your deployment. You successfully connected to MongoDB!');
+      console.info('Pinged your deployment successfully, connected to MongoDB!');
     }
   } catch (e) {
     console.error(e);
