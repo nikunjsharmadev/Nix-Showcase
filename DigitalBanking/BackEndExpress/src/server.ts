71a8036cfd8014cd0edfd72568b4e716edc4539f
Nix <nikunjsharmadev@gmail.com>
2026-07-31 04:57:25 -0300

    added https certificate

--- DigitalBanking/BackEndExpress/src/server.ts
+++ DigitalBanking/BackEndExpress/src/server.ts
@@ -1,14 +1,19 @@
-import http from 'node:http';
+import https from 'https';
+import fs from 'fs';
 import { App } from './app/index.js';
 import { handleFatalError } from './app/utils/index.js';
 import { PORT, HOSTNAME, PROCESS } from './app/constants/index.js';
 // SERVER
 async function Server() {
   try {
-    const server = http.createServer(App());
+    const options = {
+      key: fs.readFileSync('./private.key'),
+      cert: fs.readFileSync('./certificate.crt'),
+    };
+    const server = https.createServer(options, App());
     server.listen(PORT, HOSTNAME, () => {
       console.info(`server is up👍 and running🏃🏃 on:
-        Url: http://${HOSTNAME}:${PORT}`);
+            Url: https://${HOSTNAME}:${PORT}`);
     });
   } catch (err) {
     console.error(err);
