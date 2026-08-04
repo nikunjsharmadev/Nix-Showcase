149ccb4d21c3a27323f8eb21f924f875bd297866
Nix <nikunjsharmadev@gmail.com>
2026-08-04 04:51:31 -0300

    changes for compression package

--- DigitalBanking/BackEndExpress/src/app/use.ts
+++ DigitalBanking/BackEndExpress/src/app/use.ts
@@ -6,12 +6,14 @@ import { catchApiError, pageNotFound } from './utils/index.js';
 import { AppRoutes } from './routes/index.js';
 import cookieParser from 'cookie-parser';
 import rateLimit from 'express-rate-limit';
+import compression from 'compression';
 // APP USE MIDDLEWARES
 export function AppUse(app: Express) {
   app.disable('x-powered-by');
   app.use(helmet(HELMET_CONFIG));
   app.use(cookieParser());
   app.use(express.json());
+  app.use(compression());
   app.use(rateLimit(RATE_LIMIT_CONFIG));
   app.use('/api/v1', AppRoutes());
   app.use(pageNotFound);
