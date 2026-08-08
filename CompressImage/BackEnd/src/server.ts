9c2f27815ee5bc9ac65c9692b396f52b2e2b7310
Nix <nikunjsharmadev@gmail.com>
2026-08-08 12:03:31 -0300

    changes for worker file

--- CompressImage/BackEnd/src/server.ts
+++ CompressImage/BackEnd/src/server.ts
@@ -4,22 +4,18 @@ import { utilFactory } from './app/utils/index.js';
 import { constantFactory } from './app/consts/const.js';
 import { socketFactory } from './app/sockets/socket.js';
 import { queueFactory } from './app/queues/queue.js';
-import { workerFactory } from './app/workers/worker.js';
 // SERVER
 const ApiServer = async () => {
   try {
     const { HTTPS_CERTIFICATE_CONFIG, PORT, HOSTNAME, SERVER_RUN_MESSAGE } = constantFactory;
     const { setSocket, getSocket, socketEvents } = socketFactory;
     const { setQueue, setQueueEvent, monitorQueue } = queueFactory;
-    const { setWorker, workerEvents } = workerFactory;
     const server = https.createServer(HTTPS_CERTIFICATE_CONFIG, appFactory);
     setSocket(server);
     socketEvents();
     setQueue('image-processing');
     setQueueEvent('image-processing');
     await monitorQueue(getSocket());
-    setWorker('image-processing');
-    workerEvents();
     server.listen(PORT, HOSTNAME, () => console.info(SERVER_RUN_MESSAGE));
   } catch (err) {
     console.error(err);
