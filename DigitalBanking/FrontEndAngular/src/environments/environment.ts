bb258a74cdf09a268c3f597b68a31c3d0ad76785
Nix <nikunjsharmadev@gmail.com>
2026-08-14 00:23:35 -0300

    changed host

--- DigitalBanking/FrontEndAngular/src/environments/environment.ts
+++ DigitalBanking/FrontEndAngular/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const ENVIRONMENT = {
   production: true,
-  apiUrl: 'https://35.183.72.50:4001/api/v1',
+  apiUrl: 'https://3.99.131.248:4001/api/v1',
   appName: 'Digital Banking App',
   version: '1.0.0',
 } as const;
