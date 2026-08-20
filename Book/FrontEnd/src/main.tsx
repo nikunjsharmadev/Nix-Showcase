95e9d62243c1095e02e59586bb735556ca0bda29
Nix <nikunjsharmadev@gmail.com>
2026-08-20 06:19:50 -0300

    basename change

--- Book/FrontEnd/src/main.tsx
+++ Book/FrontEnd/src/main.tsx
@@ -1,13 +1,13 @@
-import "./index.css";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import HlApp from "./App";
+import './index.css';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import HlApp from './App';
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter basename="/Nix-Booking">
+    <BrowserRouter basename="/Nix-Showcase/booking/">
       <HlApp />
     </BrowserRouter>
   </QueryClientProvider>,
