e4442a847504c124157c8f4cd2a90606e097cb5d
Nix <nikunjsharmadev@gmail.com>
2026-08-30 00:59:17 -0300

    changes for booking

--- Book/FrontEnd/src/main.tsx
+++ Book/FrontEnd/src/main.tsx
@@ -1,4 +1,4 @@
-import './index.css';
+import './styles.css';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
